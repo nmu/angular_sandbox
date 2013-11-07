e2352df675eca119914abc0ae9394cb7175f9742
Ni Mu <nmu@onekingslane.com>
2013-11-07 15:48:27 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularSandbox::Application.config.secret_token = '4ac604301bc7b13e5d1ed9fcb1df183645e6e188094450fcc379ecb17f9bf234f4b11056394a3a3f56ccd840892ea72c81ef239b53be5752b3c303239af3e1e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AngularSandbox::Application.config.session_store :cookie_store, key: '_angular_sandbox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AngularSandbox::Application.config.session_store :active_record_store
